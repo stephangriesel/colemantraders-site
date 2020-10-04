06ae5d3287d344c156673baaab00eafa86175dcd
Stephan <sgriesel@gmail.com>
2020-10-04 17:01:08 +0200

    set up global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+// provider
+
+const GlobalStyle = createGlobalStyle`
+h2 {
+    color:red;
+    font-size:4rem;
+}
+`
+
+export const wrapRootElement = ({ element }) => {
+    return <>
+        <GlobalStyle />
+        {element}
+    </>
+}
