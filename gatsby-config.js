cad9cadfea1d19a610aced9fa5950bf6ef0492e7
Stephan <sgriesel@gmail.com>
2020-10-19 06:34:33 +0200

    query set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-// const queries = require("./src/constants/algolia")
+const queries = require("./src/constants/algolia")
 module.exports = {
   siteMetadata: {
     title: `Design Shop`,
@@ -70,7 +70,7 @@ module.exports = {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
         indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries: [],
+        queries: require('./src/constants/algolia'),
         chunkSize: 10000,
       }
     }
