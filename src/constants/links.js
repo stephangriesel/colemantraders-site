b04c8baf1d62780b586f7458e0ffbf80f60098dc
Stephan <sgriesel@gmail.com>
2021-03-31 07:38:07 +0200

    site set up

--- src/constants/links.js
+++ src/constants/links.js
@@ -2,57 +2,27 @@ import React from "react"
 import { FaCcStripe, FaCreditCard, FaAlipay } from "react-icons/fa"
 export default [
   {
-    page: "products",
-    label: "payments",
-    url: `/products/payments`,
+    page: "services",
+    label: "plant hire",
+    url: `/services/hire`,
     icon: <FaCreditCard className="icon" />,
-  },
-  {
-    page: "products",
-    label: "terminal",
-    url: `/products/payments`,
-    icon: <FaCreditCard className="icon" />,
-  },
-  {
-    page: "products",
-    label: "connect",
-    url: `/products/payments`,
-    icon: <FaCreditCard className="icon" />,
-  },
-  {
-    page: "products",
-    label: "billing",
-    url: `/products/payments`,
-    icon: <FaCreditCard className="icon" />,
-  },
-  {
-    page: "developers",
-    label: "documentation",
-    url: `/products/payments`,
-    icon: <FaAlipay className="icon" />,
-  },
-  {
-    page: "developers",
-    label: "libraries",
-    url: `/products/payments`,
-    icon: <FaAlipay className="icon" />,
-  },
-  {
-    page: "developers",
-    label: "plugins",
-    url: `/products/payments`,
-    icon: <FaAlipay className="icon" />,
   },
   {
     page: "company",
-    label: "jobs",
-    url: `/products/payments`,
+    label: "contact",
+    url: `/products/contact`,
     icon: <FaCcStripe className="icon" />,
   },
   {
     page: "company",
-    label: "customers",
-    url: `/products/payments`,
+    label: "about",
+    url: `/company/about`,
     icon: <FaCcStripe className="icon" />,
+  },
+  {
+    page: "contact",
+    label: "contact details",
+    url: `/products/contact`,
+    icon: <FaAlipay className="icon" />,
   },
 ]
