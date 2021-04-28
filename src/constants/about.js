683f3c85577589b8cc23d662099f85fc5c541de5
Stephan <sgriesel@gmail.com>
2021-04-28 06:58:39 +0200

    add data

---
+++ src/constants/about.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  GiCompass,
+  GiDiamondHard,
+  GiStabbedNote,
+  GiBriefcase,
+} from 'react-icons/gi';
+export default [
+  {
+    id: 1,
+    icon: <GiCompass className='icon' />,
+    label: 'Director: Johnny Coleman',
+    text:
+      'Coleman Traders (Pty) Ltd was registered in 2016, focusing on selling containers and plant hire/sales. Since the economy took it’s toll in 2020, the director started focusing on small volume diesel deliveries which posed low risk. As he evolved in the industry, he identified a demand in the market for honest and reliable wholesale services. Thanks to dedicated staff led by Johnny, the company has been able to grow in both size and reputation.',
+  },
+  {
+    id: 2,
+    icon: <GiDiamondHard className='icon' />,
+    label: 'Our Mission',
+    text:
+      'Coleman Traders (Pty) Ltd is dedicated to delivering effective, efficient, respectful services with integrity and accountability using all our proven innovative methods, and still expanding our knowledge to embrace the needs of a continuously changing industry. We proudly offer quality assurance, operational excellence and a fanatical concern for our customer’s needs whilst complying with all environmental and employee development responsibilities.',
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote className='icon' />,
+    label: 'Our Vision',
+    text:
+      'Our key success factors are strategic focus, people, operations, marketing and finances. Depending on these factors we strive to be recognised as a premier Fuel Wholesale, Plant Hire and Containers distributing company in South Africa.',
+  },
+];
--- src/pages/company/about.js
+++ src/pages/company/about.js
@@ -1,28 +1,86 @@
-import React from "react"
-import { Layout } from "../../components"
-import styled from "styled-components"
-import { Link } from "gatsby"
+import React from 'react';
+import { Layout } from '../../components';
+import styled from 'styled-components';
+import Title from '../../components/Title';
+import about from '../../constants/about';
+// import { Link } from 'gatsby';
 const About = (props) => {
-    const path = props.location.pathname.slice(1);
-    console.log("Sub Page Props:", props);
-    return (
-        <Layout>
-            <Wrapper>
-                <div>
-                    <h1>{path}</h1>
-                    <Link to="/" className="btn">
-                        About
-        </Link>
-                </div>
-            </Wrapper>
-        </Layout>
-    )
-}
+  // const path = props.location.pathname.slice(1);
+  // console.log('Sub Page Props:', props);
+  return (
+    <Layout>
+      <Wrapper className='about'>
+        <div className='section-center'>
+          <Title title='About Us' />
+          {about.map(({ id, icon, label, text }) => {
+            return (
+              <article key={id}>
+                <span>{icon}</span>
+                <h4>{label}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+        <div>
+          <span>Icon</span>
+          <h4>Unique Approach</h4>
+          <p>
+            The Why:
+            <ul>
+              <li>
+                Empower People Today to Build a Better Future for the Next
+                Generation
+              </li>
+            </ul>
+          </p>
+          <p>
+            The Hows:
+            <ul>
+              <li>Do the right thing, the first time - Always</li>
+              <li>Lead by example, follow ethically</li>
+              <li>Bring out the best in everyone</li>
+              <li>Make them enquire again</li>
+              <li>Embrace transparency</li>
+              <li>Do it all in the spirit of Coleman Traders</li>
+            </ul>
+          </p>
+          <p>
+            The Whats:
+            <ul>
+              <li>Identify & invest in improvement & development</li>
+              <li>Delight our customers</li>
+              <li>Honour our commitments</li>
+              <li>Incorporate new technology</li>
+              <li>Improve financial security</li>
+            </ul>
+          </p>
+        </div>
+        <div>
+          <span>Icon</span>
+          <h4>Core Values</h4>
+          <p>
+            <ul>
+              <li>Commitment to customers above all else</li>
+              <li>We can generate profit without acting unethically</li>
+              <li>Great just isn’t good enough</li>
+              <li>Connect with transparency, dignity and respect</li>
+              <li>Act with courage</li>
+              <li>Awareness of social and physical environment</li>
+              <li>We look beyond the present to deliver future value</li>
+            </ul>
+          </p>
+        </div>
+      </Wrapper>
+    </Layout>
+  );
+};
 
 const Wrapper = styled.main`
-  margin-top: -5rem;
+  /* margin-top: 2rem; */
   background: var(--clr-primary-8);
-  min-height: calc(100vh - 5rem);
+  /* height: auto; */
+  /* min-height: calc(100vh - 5rem); */
   display: grid;
   place-items: center;
 
@@ -40,6 +98,6 @@ const Wrapper = styled.main`
       font-size: 4rem;
     }
   }
-`
+`;
 
-export default About
+export default About;
