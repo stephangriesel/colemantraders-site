e6a6cc2e6e6dc044e843296db7873fbf60fcf800
Stephan <sgriesel@gmail.com>
2021-08-22 08:03:46 +0200

    loader test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
-import { wrapRootElement as wrap } from './root-wrapper';
+import {wrapRootElement as wrap} from './root-wrapper';
+
+export const onInitialClientRender = () => {
+  setTimeout(function () {
+    // document.getElementById('___loader').style.display = 'none';
+  }, 1000);
+};
 
 export const wrapRootElement = wrap;
